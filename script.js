eeed4dd9d257c6a4125ffccb74f47beae03ec680
Naufal <naufalramadhan1324@gmail.com>
2023-04-29 18:33:23 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const shareOption = document.querySelector(".share-option");
+const shareButton = document.querySelector(".share-button");
+
+shareButton.addEventListener("click", function () {
+  this.classList.toggle("active");
+  shareOption.classList.toggle("active");
+});
+
